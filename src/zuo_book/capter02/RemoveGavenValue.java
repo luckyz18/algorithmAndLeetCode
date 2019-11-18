d84d319964ceed9afa8e6f420014f06b306e3239
lihua3 <738183495@qq.com>
2019-11-18 17:01:54 +0800

    在单链表中删除指定值的节点

---
+++ src/zuo_book/capter02/RemoveGavenValue.java
@@ -0,0 +1,43 @@
+package zuo_book.capter02;
+
+public class RemoveGavenValue {
+
+    static class Node {
+        int value;
+        Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node removeGivenValue(Node head, int k){
+        if (head == null ){
+            return null;
+        }
+        Node pre = new Node(0);
+        pre.next = head;
+        Node newHead = pre;
+        Node cur = head;
+        while(cur != null){
+            if (cur.value  ==  k){
+                pre.next = cur.next;
+                cur = cur.next;
+            }else{
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return newHead.next;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.next = new Node(1);
+        //head.next.next.next = new Node(2);
+        removeGivenValue(head,1);
+    }
+
+
+}
