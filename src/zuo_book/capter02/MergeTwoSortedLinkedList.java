50d7cb58da6507dd554e724798726ff636152606
lihua3 <738183495@qq.com>
2019-11-20 21:45:21 +0800

    按照左右分区的方式重新组合链表

--- src/zuo_book/capter02/MergeTwoSortedLinkedList.java
+++ src/zuo_book/capter02/MergeTwoSortedLinkedList.java
@@ -37,11 +37,11 @@ public class MergeTwoSortedLinkedList {
     }
 
     public static void main(String[] args) {
-        //Node head1 = new Node(1);
-        //head1.next = new Node(5);
-        //head1.next.next = new Node(6);
+        Node head1 = new Node(1);
+        head1.next = new Node(5);
+        head1.next.next = new Node(6);
 
-        Node head1 = new Node(9);
+        //Node head1 = new Node(9);
 
         Node head2 = new Node(2);
         head2.next = new Node(3);
