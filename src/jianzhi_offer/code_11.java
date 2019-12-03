4e97b5ac18729451f89f7f8592986c9eb45fe8d9
lihua3 <738183495@qq.com>
2019-12-03 14:57:27 +0800

    回溯一脸茫然

--- src/jianzhi_offer/code_11.java
+++ src/jianzhi_offer/code_11.java
@@ -12,6 +12,9 @@ public class code_11 {
         int l = 0;
         while (r > l) {
             int mid = ((r - l) >> 1) + l;
+            if (array[l] == array[r] && array[mid] == array[l]){
+                return minNumber(array, l, r);
+            }
             if (array[mid] <= array[r]) {
                 r = mid;
             } else {
@@ -20,27 +23,45 @@ public class code_11 {
         }
         return array[l];
     }
+
+    private static int minNumber(int[] array, int l, int r) {
+        int res = array[l];
+        for (int i = l+1; i <=r ; i++) {
+            if (array[i] < res){
+                res = array[i];
+            }
+        }
+        return res;
+    }
+
+    //ac 不过 没考虑 1 2 3 4  这种情况
+    //还是没通过 不知什么鬼。。。
+    //通过了
     public static int minNumberInRotateArray2(int[] array) {
         if (array.length == 0) {
             return 0;
         }
         int r = array.length - 1;
         int l = 0;
-        while (r > l+1) {
-            int mid = ((r - l) >> 1) + l;
-            if (array[mid] > array[l]){
+        int mid =l;
+        while (array[l] >= array[r]) {
+            if (r == l+1){
+                mid = r;
+                break;
+            }
+            mid = (l+r)/2;
+            if (array[mid] >= array[l]){
                 l = mid;
-                continue;
             } else {
                 r = mid;
             }
         }
-        return array[r];
+        return array[mid];
     }
 
 
     public static void main(String[] args) {
-        int i = minNumberInRotateArray2(new int[]{});
+        int i = minNumberInRotateArray2(new int[]{1,2,3,4});
         System.out.println(i);
     }
 }
